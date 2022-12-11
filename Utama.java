391d33e7cac87e0935ff76128d523d21271d8993
wisnuwicaksn <adiwisnu123@gmail.com>
2022-12-11 15:41:09 +0700

    add gambar

--- Utama.java
+++ Utama.java
@@ -6,10 +6,13 @@ public class Utama {
         BangunDatar lingkaran = new Lingkaran(5);
         
         persegi.setWarna("Merah");
+        persegi.Gambar();
         System.out.println("Luas persegi " + persegi.getWarna() + " : " + persegi.getLuas());
         segitiga.setWarna("Hitam");
-        System.out.println("Luas segitiga " + segitiga.getWarna() + " : " + segitiga.getLuas());
+        segitiga.Gambar();
+        System.out.println("Luas Segitiga " + segitiga.getWarna() + " : " + segitiga.getLuas());
         lingkaran.setWarna("Hijau");
+        lingkaran.Gambar();
         System.out.println("Luas lingkaran " + lingkaran.getWarna() + " : " + lingkaran.getLuas());
     }
 }
